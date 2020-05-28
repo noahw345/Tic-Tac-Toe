c2cc227fc7dacfef6004e1b477a6de384701e530
Noah Weinstein <nwweinstein3@gmail.com>
2020-05-28 14:04:39 -0400

    intial commit

--- Tic Tac Toe/src/comWeinstein/Game.java
+++ Tic Tac Toe/src/comWeinstein/Game.java
@@ -35,8 +35,6 @@ public class Game {
         }
     }
 	
-	// Loop through all cells of the board and if one is found to be empty (contains char '-') then return false.
-    // Otherwise the board is full.
     public boolean isBoardFull() {
         boolean isFull = true;
 		
@@ -51,15 +49,10 @@ public class Game {
         return isFull;
     }
 	
-	
-    // Returns true if there is a win, false otherwise.
-    // This calls our other win check functions to check the entire board.
     public boolean checkForWin() {
         return (checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin());
     }
 	
-	
-    // Loop through rows and see if any are winners.
     private boolean checkRowsForWin() {
         for (int i = 0; i < 3; i++) {
             if (checkRowCol(board[i][0], board[i][1], board[i][2])) {
@@ -69,8 +62,7 @@ public class Game {
         return false;
     }
 	
-	
-    // Loop through columns and see if any are winners.
+
     private boolean checkColumnsForWin() {
         for (int i = 0; i < 3; i++) {
             if (checkRowCol(board[0][i], board[1][i], board[2][i])) {
@@ -81,19 +73,16 @@ public class Game {
     }
 	
 	
-    // Check the two diagonals to see if either is a win. Return true if either wins.
     private boolean checkDiagonalsForWin() {
         return ((checkRowCol(board[0][0], board[1][1], board[2][2])) || (checkRowCol(board[0][2], board[1][1], board[2][0])));
     }
 	
 	
-    // Check to see if all three values are the same (and not empty) indicating a win.
     private boolean checkRowCol(char c1, char c2, char c3) {
         return ((c1 != '-') && (c1 == c2) && (c2 == c3));
     }
 	
 	
-    // Change player marks back and forth.
     public void changePlayer() {
         if (currentMark == 'x') {
             currentMark = 'o';
@@ -104,10 +93,7 @@ public class Game {
     }
 	
 	
-    // Places a mark at the cell specified by row and col with the mark of the current player.
     public boolean placeMark(int row, int col) {
-		
-        // Make sure that row and column are in bounds of the board.
         if ((row >= 0) && (row < 3)) {
             if ((col >= 0) && (col < 3)) {
                 if (board[row][col] == '-') {
@@ -116,7 +102,6 @@ public class Game {
                 }
             }
         }
-		
         return false;
     }
 }
